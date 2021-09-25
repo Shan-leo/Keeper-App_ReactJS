a52808d003a7dbf1eb99a80349a9ba44bd1ac9eb
Tharindu Dhanushka(Shan Leo) <76651361+Shan-leo@users.noreply.github.com>
2021-09-25 14:46:31 +0530

    first commit

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./Heading";
+import Footer from "./footer";
+import Note from './Note';
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
